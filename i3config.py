b53dc42c2963d71538483b99c940f0d7eb247bfc
David96 <davidlepplaweber@aospa.co>
2016-04-13 14:46:56 +0200

    Check for __main__

--- i3config.py
+++ i3config.py
@@ -7,15 +7,16 @@ from config import Config
 
 import os
 
-c = Config()
+if __name__ == "__main__":
+    c = Config()
 
-if len(sys.argv) != 2:
-    print("Usage: %s theme-name")
-    print("The current theme is %s" % c.get("theme"))
-    sys.exit(-1)
+    if len(sys.argv) != 2:
+        print("Usage: %s theme-name")
+        print("The current theme is %s" % c.get("theme"))
+        sys.exit(-1)
 
-logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.DEBUG)
+    logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.DEBUG)
 
-themes = Themes()
-t = themes.load_theme(sys.argv[1])
-t.apply()
+    themes = Themes()
+    t = themes.load_theme(sys.argv[1])
+    t.apply()
